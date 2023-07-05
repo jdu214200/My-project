c46f2df7fbd909d1162d90597aff9ebb34257ae0
Islom <123618830+jdu214200@users.noreply.github.com>
2023-07-05 22:32:15 +0300

    Add files via upload

---
+++ 20/echi.js
@@ -0,0 +1,15 @@
+Berikut adalah contoh kode JavaScript:
+
+```
+// Mendapatkan input dari pengguna
+var nama = prompt("Masukkan nama Anda:");
+
+// Menampilkan pesan selamat datang dengan menggunakan input pengguna
+console.log("Selamat datang, " + nama + "!");
+
+// Menghitung jumlah karakter dalam input pengguna
+var jumlahKarakter = nama.length;
+console.log("Jumlah karakter dalam nama Anda adalah " + jumlahKarakter + ".");
+```
+
+Kode di atas akan meminta pengguna untuk memasukkan namanya, kemudian akan menampilkan pesan selamat datang dengan nama yang dimasukkan dan jumlah karakter dalam nama tersebut.
